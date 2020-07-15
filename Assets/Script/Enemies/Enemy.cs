d3254c2095597f473cc947a938a14be1b83cc39d
Elton Silva <eltoncarsilva73@gmail.com>
2020-07-15 14:33:28 -0300

    enemies controller

---
+++ Assets/Script/Enemies/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float m_speedRun;
+    [SerializeField] private Transform m_CheckGrounded;
+    [SerializeField] private LayerMask m_groundLayer;
+    [SerializeField] private float m_checkRadio;
+
+    Rigidbody2D _Rigidbody;
+    Animator _Animator;
+
+    bool _right;
+
+    void Start()
+    {
+        _right = true;
+        _Rigidbody = transform.GetComponent<Rigidbody2D>();
+        _Animator = transform.GetComponent<Animator>();
+    }
+
+
+    bool _isIlive = false;
+    bool _wasBorn = false;
+    void Update()
+    {
+        if (_wasBorn && !_isIlive)
+            _Animator.SetBool("isBorning", true);    
+    }
+
+    private void FixedUpdate()
+    {
+        this.GuardMode();
+        this.CheckGrounded();
+        this.Flip();
+    }
+    
+    private void OnBecameVisible()
+    {
+        _isGuardMode = true;
+        if (!_isIlive)
+            _wasBorn = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        _isGuardMode = false;
+    }
+
+    bool _isGuardMode;
+    void GuardMode()
+    {
+        if (_isGuardMode && _isIlive)
+        {
+            if (_right)
+                _Rigidbody.velocity = new Vector2(m_speedRun, _Rigidbody.velocity.y);
+            else
+                _Rigidbody.velocity = new Vector2(-m_speedRun, _Rigidbody.velocity.y);
+        }
+    }
+
+
+    void CheckGrounded()
+    {
+        if (!Physics2D.OverlapCircle(m_CheckGrounded.position, m_checkRadio, m_groundLayer))
+            _right = !_right;
+    }
+    
+    void Flip()
+    {
+        if (_right)
+            transform.localScale = new Vector3(1, 1, 1);
+        else
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void SetIliveEnemy()
+    {
+        _isIlive = true;
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float _SpeedRun;
+    [SerializeField] private float m_speedRun;
+    [SerializeField] private float m_speedJump;
+    [SerializeField] private float m_jumpForce = 0.5f;
     private Rigidbody2D _Rigidbody;
     private Animator _Animator;
     
     private bool AttakingPressed = false;
+    private bool JumpPressed = false;
 
     void Start()
     {
@@ -18,12 +21,6 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
-    }
-
-    void FixedUpdate()
-    {
-
         if (Input.GetButtonDown("Attack") && !_Animator.GetBool("isAttacking") && !AttakingPressed)
         {
             _Animator.SetTrigger("isAttacking");
@@ -32,31 +29,81 @@ public class Player : MonoBehaviour
         if (Input.GetButtonUp("Attack"))
             AttakingPressed = false;
 
-        float _speed = 0;
+        _speed = 0;
         if (Input.GetAxisRaw("Horizontal") > 0 && !_Animator.GetBool("isAttacking"))
-        {
-            _Animator.SetBool("isRunning", true);
-            _speed = _SpeedRun;
-        }
+            this.RunAnimation(m_speedRun);
         else if (Input.GetAxisRaw("Horizontal") < 0 && !_Animator.GetBool("isAttacking"))
-        {
-            _Animator.SetBool("isRunning", true);
-            _speed = -_SpeedRun;
-        }
+            this.RunAnimation(-m_speedRun);
         else
             _Animator.SetBool("isRunning", false);
+
+        if (Input.GetButtonDown("Jump") && _grounded && !JumpPressed)
+        {
+            _Rigidbody.AddForce(new Vector2(0, m_jumpForce), ForceMode2D.Impulse);
+            JumpPressed = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+            JumpPressed = false;
+
         
-            _Rigidbody.velocity = new Vector2(_speed, _Rigidbody.velocity.y);
+        this.CheckGround();
+    }
+
+    void RunAnimation(float speed)
+    {
+        if (speed > 0)
+            if (_Animator.GetBool("isJumping"))
+                _speed = m_speedJump;
+            else
+                _speed = m_speedRun;
+        else if (speed < 0)
+            if (_Animator.GetBool("isJumping"))
+                _speed = -m_speedJump;
+            else
+                _speed = -m_speedRun;
+            
+
+        if (!_Animator.GetBool("isJumping"))
+            _Animator.SetBool("isRunning", true);
+        else
+            _Animator.SetBool("isRunnig", false);
+    }
+
+    private float _speed;
+    void FixedUpdate()
+    {   
+        _Rigidbody.velocity = new Vector2(_speed, _Rigidbody.velocity.y);
 
         this.Flip();
     }
-
+    
     void Flip()
     {
         if (_Rigidbody.velocity.x > 0)
             transform.localScale = new Vector3(1, 1, 1);
         else if (_Rigidbody.velocity.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+
+    [SerializeField] private float m_distanceCollision = 0.5f;
+    [SerializeField] private LayerMask m_groundLayer;
+
+    bool _grounded = false;
+    void CheckGround()
+    {
+        _grounded = false;
+        Vector2 position = transform.position;
+        Vector2 directionDown = new Vector2(0, -m_distanceCollision);
+
+        Debug.DrawRay(position, directionDown, Color.green);
+
+        RaycastHit2D hit = Physics2D.Raycast(position, directionDown, m_distanceCollision, m_groundLayer);
+        if (hit.collider != null)
+        {
+            _grounded = true;
+        }
+        _Animator.SetBool("isJumping", !_grounded);
     }
     
 }
